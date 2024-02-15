7f29d9dbb5bd3074aa28c51bbc3119027f77926e
Austin Graham <austiingraham@gmail.com>
2024-02-14 22:02:09 -0500

    added getUserById method to userController.js for retrieving a single user by ID

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,22 @@ const userController = {
       res.status(500).json(error);
     }
   },
+
+  async getUserById(req, res) {
+    try {
+      const user = await User.findById(req.params.userId)
+        .populate('thoughts')
+        .populate('friends');
+
+        if (!user) {
+          return res.status(404).json({ message: 'No user with that ID' });
+        }
+        
+      res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 }
 
 module.exports = userController;
