2577bab9aa98490aadd095ea813988d1e559c15c
Austin Graham <austiingraham@gmail.com>
2024-02-14 21:37:37 -0500

    added userController.js with getAllUsers method for retrieving all users from the database

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+
+const userController = {
+  async getAllUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+}
+
+module.exports = userController;
