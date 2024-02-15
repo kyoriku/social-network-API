ac4565e4128ae0ec7ba8d72d4520930f86097609
Austin Graham <austiingraham@gmail.com>
2024-02-15 03:02:17 -0500

    added updateThought method for updating a thought by ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -53,6 +53,25 @@ const thoughtController = {
       res.status(500).json(err);
     }
   },
+
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this ID!' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = thoughtController;
