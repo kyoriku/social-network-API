ab4b8e50a48fe7f978564eac7457f78abf4d5b83
Austin Graham <austiingraham@gmail.com>
2024-02-15 01:26:09 -0500

    added getThoughtById method for retrieving a single thought by ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -15,6 +15,21 @@ const thoughtController = {
       res.status(500).json(err);
     }
   },
+
+  // Method for getting a single thought by ID
+  async getThoughtById(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with that ID' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
 
 module.exports = thoughtController;
