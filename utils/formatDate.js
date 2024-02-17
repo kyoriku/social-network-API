1c7568f9cd870f49a6ff81bafeb09f47400409e0
Austin Graham <austiingraham@gmail.com>
2024-02-17 05:25:41 -0500

    added comments to formatDate.js for improved code documentation

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,20 +1,28 @@
+// Defining a function called formatDate which takes a timestamp parameter
 function formatDate(timestamp) {
+  // Defining options for formatting the date of the timestamp
   const dateOptions = {
     month: 'short',
     day: 'numeric',
     year: 'numeric',
   };
 
+  // Defining options for formatting the time of the timestamp
   const timeOptions = {
     hour: 'numeric',
     minute: 'numeric',
     hour12: true
   }
 
+  // Creating a new Date object from the provided timestamp
   const date = new Date(timestamp);
+  // Formatting the date of the timestamp using the defined date options
   const formattedDate = date.toLocaleDateString('en-US', dateOptions);
+  // Formatting the time of the timestamp using the defined time options
   const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
+  // Returning a string that combines the formatted date and time
   return `${formattedDate} at ${formattedTime}`;
 }
 
-module.exports = formatDate
+// Exporting the formatDate function to make it available for use in other modules
+module.exports = formatDate;
