a0b3bad96945bae6a8ed6065fe8b2beff4ec146a
Austin Graham <austiingraham@gmail.com>
2024-02-18 01:09:48 -0500

    added comments to connection.js in the config directory, and server.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
+// Load environment variables from a .env file into process.env
 require('dotenv').config();
+
+// Destructure the 'connect' and 'connection' objects from the 'mongoose' module
 const { connect, connection } = require('mongoose');
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SocialNetworkDB';
+// Define the MongoDB connection string, using the value of MONGODB_URI environment variable if available, otherwise default to a local MongoDB instance
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SocialNetworkDB';
 
+// Establish a connection to the MongoDB database using the connection string
 connect(connectionString);
 
+// Export the 'connection' object for external use, which represents the current connection to the MongoDB database
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+// Importing the express module
 const express = require('express');
+
+// Importing the database connection from the config directory
 const db = require('./config/connection');
+
+// Importing the routes from the routes directory
 const routes = require('./routes');
 
+// Defining the port number to listen on, using the environment variable PORT if available, otherwise defaulting to 3001
 const PORT = process.env.PORT || 3001;
+
+// Creating an instance of the Express application
 const app = express();
 
+// Middleware to parse incoming request bodies with urlencoded payloads
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse incoming request bodies with JSON payloads
 app.use(express.json());
+
+// Mounting the routes onto the application
 app.use(routes);
 
+// Establishing a connection to the database, and once the connection is open, starting the Express server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
